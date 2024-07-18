110e8b058d77be6094ca062d074b9f59da5b3e5f
Selin Özkan <selinnozkann5@gmail.com>
2024-07-18 10:31:54 +0300

    initial commit

---
+++ src/main/java/store/mgmt/MgmtApplication.java
@@ -0,0 +1,13 @@
+package store.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmtApplication.class, args);
+	}
+
+}
