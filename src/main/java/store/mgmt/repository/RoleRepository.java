af3c1e3581b488b41836266bd037b8dd5c327cdc
Selin Özkan <selinnozkann5@gmail.com>
2024-07-28 16:02:20 +0300

    repository annotation

--- src/main/java/store/mgmt/repository/RoleRepository.java
+++ src/main/java/store/mgmt/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package store.mgmt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import store.mgmt.entity.Role;
 import store.mgmt.entity.UserRole;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByRoleName(UserRole name);
 }
